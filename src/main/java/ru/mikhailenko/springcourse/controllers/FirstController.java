8633ab3c2f91fdb3f722407faba13c9b52c401f6
AleksMikhailenko <maa1404@mail.ru>
2021-05-16 00:49:52 +0300

    change first controller

--- src/main/java/ru/mikhailenko/springcourse/controllers/FirstController.java
+++ src/main/java/ru/mikhailenko/springcourse/controllers/FirstController.java
@@ -3,17 +3,25 @@ package ru.mikhailenko.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String firstName = request.getParameter("name");
+        String lastName = request.getParameter("surname");
+        System.out.println(String.format("Hello, %s %s", firstName, lastName));
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    public String goodByePage(@RequestParam(value = "name", required = false) String firstName,
+                              @RequestParam(value = "surname", required = false) String lastName) {
+        System.out.println(String.format("Hello, %s %s", firstName, lastName));
         return "first/goodbye";
     }
 }

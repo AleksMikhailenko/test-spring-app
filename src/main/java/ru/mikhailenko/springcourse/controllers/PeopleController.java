b07199ea81b170ab0755198a9183c8a19dbb1233
AleksMikhailenko <maa1404@mail.ru>
2021-05-16 19:15:54 +0300

    add create operation for app

--- src/main/java/ru/mikhailenko/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/mikhailenko/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.mikhailenko.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.mikhailenko.springcourse.dao.PersonDAO;
+import ru.mikhailenko.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -36,5 +35,17 @@ public class PeopleController {
                        Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
